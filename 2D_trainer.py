2cd9b041842c85a81d1656f15ae737491ffb8072
gozalg <gabrielg@campus.technion.ac.il>
2024-10-22 12:24:55 +0300

    2d trainer

--- 2D_trainer.py
+++ 2D_trainer.py
@@ -429,7 +429,8 @@ def main(split =3,upload =False,save_features=False):
     else:
         # output_folder = os.path.join(args.out, args.dataset, args.exp + "_" + datetime.datetime.now().strftime("%Y%m%d"),
         #                               str(split), datetime.datetime.now().strftime("%H%M"))
-        output_folder = os.path.join(args.out, args.dataset, f"{args.task}_{args.num_classes}_{datetime.datetime.now().strftime("%Y%m%d")}", str(split))
+        cur_date = datetime.datetime.now().strftime("%Y%m%d_%H%M")
+        output_folder = os.path.join(args.out, args.dataset, f"{args.task}_{args.num_classes}_{cur_date}", str(split))
         os.makedirs(output_folder, exist_ok=True)
 
     checkpoint_file = os.path.join(output_folder, "checkpoint" + ".pth.tar")
