17fd83b85298dce806720e3dcaa2e15c006359e8
gozalg <gabrielg@campus.technion.ac.il>
2025-05-08 22:02:16 +0300

    removed XL option

--- utils_3D/train_opts_3D.py
+++ utils_3D/train_opts_3D.py
@@ -134,8 +134,8 @@ parser.add_argument('--corner_cropping', type='bool', default=True,
 # ----------------------
 parser.add_argument('--arch', type=str, default="X3D", choices=["EfficientNetV2", "X3D"],
                     help="Network architecture.")
-parser.add_argument('--arch_size', type=str, default="L", choices=["XS", "S", "M", "L", "XL"],
-                    help="Network architecture size as follows:\n\tX3D choices: [XS, S, M, L, XL].\n\tEfficientnetV2 choices: [S, M, L].")
+parser.add_argument('--arch_size', type=str, default="L", choices=["XS", "S", "M", "L"],
+                    help="Network architecture size as follows:\n\tX3D choices: [XS, S, M, L].\n\tEfficientnetV2 choices: [S, M, L].")
 parser.add_argument('--use_resnet_shortcut_type_B', type='bool', default=False,
                     help="Whether to use shortcut connections of type B.") # TODO: remove
 parser.add_argument('--input_size', type=int, default=224, help="Target size (width/ height) of each frame.")
